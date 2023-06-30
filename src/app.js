d43984c947db7d2948230294f4fec722a20469eb
Eduardo André <eduardo_andre@id.uff.br>
2023-06-30 11:46:45 -0300

    alterações

--- src/app.js
+++ src/app.js
@@ -1,20 +1,40 @@
 import  express  from "express";
 import cors from "cors"
 import { MongoClient } from "mongodb"
-import dotenv from "dotenv";
-dotenv.config();
+import dayjs from "dayjs"; 
+import Joi from "joi";
+
+
+//url encrypty
+//
 
 // Api construção
 const app = express()
 app.use(cors())
 app.use(express.json())
+//
 
-// Server mongo reading and editing  creation 
+// Server mongo reading and editing by api
 const mongoClient = new MongoClient("mongodb://127.0.0.1:27017/test");
 let db = mongoClient.db();
 mongoClient.connect()
 .then(() => {db = mongoClient.db();console.log("server is working")})
 .catch((err) => console.log(err.message,"não foi "));
+//
+app.post("/participants",(req,res)=>{
+    const name = req.body
+    const validname = Joi.object({
+        name: Joi.string().required()
+    })
+    const valited = validname.validate(name,{ abortEarly: false })
+    console.log(valited)
+    res.send(valited)
+})
+app.get("/participants",(req,res)=>{})
+
+app.post("/messages",(req,res)=>{})
+app.get("/messages",(req,res)=>{})
+
 
 // Api reading 
 app.listen(5000,()=>console.log("api is working"))
